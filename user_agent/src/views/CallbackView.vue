c63623e555011bca11cccf244838464c475e3792
Dirk van Zon <dirk@moeilijkedingen.nl>
2024-10-13 15:32:05 +0200

    Cleaned up callback view

--- user_agent/src/views/CallbackView.vue
+++ user_agent/src/views/CallbackView.vue
@@ -1,19 +1,15 @@
 <script setup>
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
 import { onMounted } from 'vue';
 import { useAuthStore } from "@/store"
 
-const { session_state, iss, code } = useRoute().query
+const { _, __, code } = useRoute().query
 const authStore = useAuthStore();
 
 onMounted(async () => {
   await authStore.connect(code)
 })
-
 </script>
 
 <template>
-  <div class="callback">
-    <h1>This is an callback page</h1>
-  </div>
 </template>
