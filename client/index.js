729249a00c98ce03fb692d2fda94516507b5d9ba
Dirk van Zon <dirk.v.zon@outlook.com>
2025-03-12 11:07:02 +0100

    fix: CORS issue in client

--- client/index.js
+++ client/index.js
@@ -10,14 +10,15 @@ dotenv.config();
 const app = express();
 const PORT = 5000
 
+app.options('*', cors())
+app.use(cors({
+    origin: [process.env.USER_AGENT_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true
+}));
+
 app.use(bodyParser.json());
 app.use(authRoutes)
 app.use(travelRoutes)
-
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true
-  }));
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
